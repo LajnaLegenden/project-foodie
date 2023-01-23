ee1691d3f77c956073fab5930985aa6fb09e7563
Linus Jansson <00jansson@gmail.com>
2023-01-23 22:49:42 +0100

    Init blazor project

---
+++ project-foodie/Model/Forecast.cs
@@ -0,0 +1,8 @@
+namespace project_foodie.Model;
+
+public class WeatherForecast
+{
+    public DateTimeOffset Date { get; set; }
+    public int TemperatureCelsius { get; set; }
+    public string? Summary { get; set; }
+}
