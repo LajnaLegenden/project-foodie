0dfd231aee6d2b5b57431f6c7f559f28395442aa
Linus Jansson <00jansson@gmail.com>
2023-02-07 04:05:51 +0100

    Change url method in confirm.py to use the new url

--- scripts/pipeline/confirm.py
+++ scripts/pipeline/confirm.py
@@ -4,7 +4,7 @@ import json
 import requests
 import util as u
 import time as time
-
+import sys
 def main():
     print("Starting confirm")
     branch = u.getGitBranch()
@@ -15,11 +15,12 @@ def main():
         print("Skipping confirm")
         exit(0)
     # make sure the server responds with a 200 giving it 20 attempts and 3 seconds between each attempt
-    url = "http://locahost:" + str(config['port']) + "/"
+    url = config['url']
 
     num_attempts = 20
     while num_attempts > 0:
         try:
+            print("Attempting to connect to server: " + url + " attempt: " + str(20-num_attempts) + "/20")
             response = requests.get(url)
             if response.status_code == 200:
                 break
@@ -29,7 +30,7 @@ def main():
         time.sleep(3)
     if num_attempts == 0:
         print("Server failed to start")
-        sys.exit(1)
+        exit(1)
     print("Server started successfully")
 
 
