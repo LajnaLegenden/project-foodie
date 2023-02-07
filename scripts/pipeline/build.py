b7a69b46ada0576eace6a9da6845ddb8e95de1d3
Linus Jansson <00jansson@gmail.com>
2023-02-07 03:11:18 +0100

    Change line end to LF

--- scripts/pipeline/build.py
+++ scripts/pipeline/build.py
@@ -5,13 +5,18 @@ import subprocess
 import json
 import docker
 import util as u
+
+
 def main():
     branch = u.getGitBranch()
     config = u.getConfig(branch)
-    print("Starting build for branch " + branch + " and hostname " + config['hostname'])
+    print("Starting build for branch " + branch +
+          " and hostname " + config['hostname'])
     client = docker.APIClient(base_url=config['daemon'])
     # print stream of build output as it happens
     for line in client.build(path="./", tag=u.getImageTag(config)):
         u.printLine(line)
+
+
 if __name__ == "__main__":
     main()
