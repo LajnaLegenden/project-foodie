3a0ea0fd4855d6e5260c149eca5e207987d886a1
Linus Jansson <00jansson@gmail.com>
2023-02-14 13:53:29 +0100

    Work

--- scripts/pipeline/build.py
+++ scripts/pipeline/build.py
@@ -14,26 +14,8 @@ def main():
           " and hostname " + config['hostname'])
     client = docker.APIClient(base_url=config['daemon'])
 
-
-    # get jenkins home directory from environment
-    jenkins_home = os.environ['JENKINS_HOME']
-    # cehck if jenkinshomee/env exists
-    if not os.path.exists(jenkins_home + "/env"):
-        os.makedirs(jenkins_home + "/env")
-    #check if env file exists
-    if not os.path.isfile(jenkins_home + "/env/" + config['env']):
-        # create env file
-        with open(jenkins_home + "/env/" + config['env'], 'w') as env_file:
-            env_file.write("")
-    #add env to container
-    env = []
-    with open(jenkins_home + "/env/" + config['env'], 'r') as env_file:
-        for line in env_file:
-            env.append(line.rstrip())
-
-
     # print stream of build output as it happens
-    for line in client.build(path="./", tag=u.getImageTag(config),environment=env):
+    for line in client.build(path="./", tag=u.getImageTag(config)):
         u.printLine(line)
 
 
