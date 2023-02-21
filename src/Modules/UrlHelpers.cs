de7e9dec132bef71ef4bf783d8c3a6d03f50434d
Linus Jansson <00jansson@gmail.com>
2023-02-21 11:49:25 +0100

    DayMenu and UrlHelper Modueles

---
+++ src/Modules/UrlHelpers.cs
@@ -0,0 +1,27 @@
+using System.Web;
+
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace project_foodie.Modules
+{
+    public static class UrlHelper
+    {
+        public static string RemoveQueryStringByKey(string url, string key)
+        {
+            var uri = new Uri(url);
+
+            // this gets all the query string key value pairs as a collection
+            var newQueryString = HttpUtility.ParseQueryString(uri.Query);
+
+            // this removes the key if exists
+            newQueryString.Remove(key);
+
+            // this gets the page path from root without QueryString
+            string pagePathWithoutQueryString = uri.GetLeftPart(UriPartial.Path);
+
+            return newQueryString.Count > 0
+                ? String.Format("{0}?{1}", pagePathWithoutQueryString, newQueryString)
+                : pagePathWithoutQueryString;
+        }
+    }
+}
