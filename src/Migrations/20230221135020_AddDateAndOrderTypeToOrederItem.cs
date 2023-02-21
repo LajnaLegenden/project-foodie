0e25f13662a8ee67460986582f64ef7864b897cb
Veronica Axelsson <veronica-a@live.se>
2023-02-21 14:53:19 +0100

    update OrderItem

---
+++ src/Migrations/20230221135020_AddDateAndOrderTypeToOrederItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projectfoodie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateAndOrderTypeToOrederItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "date",
+                table: "OrderItem",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "OrderItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "date",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "OrderItem");
+        }
+    }
+}
