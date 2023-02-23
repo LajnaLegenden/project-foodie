c8973967f6ddb465cc73a09ad505c90cefd96489
TinkzBot <zakaria.ziadi92@gmail.com>
2023-02-23 21:58:02 +0100

    Added Comments To My Functions / Variables.

--- src/Services/CartService.cs
+++ src/Services/CartService.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using Newtonsoft.Json;
 
+// Cart service Class to handle the cart count
 public class CartService
 {
     private readonly ILocalStorageService _localStorage;
@@ -9,26 +10,28 @@ public class CartService
     {
         _localStorage = localStorage;
     }
-
+    // Cart count variable
     private int cartCount;
+    // Event handler to notify the cart count change
     public event EventHandler CartCountChanged;
 
+    // Get and Set the cart count
     public int GetCartCount()
     {
         return cartCount;
     }
-
+    // Set the cart count
     public void SetCartCount(int count)
     {
         cartCount = count;
     }
-
+    // Add to cart method
     public async void AddToCart()
     {
         cartCount = await GetObjectCount();
         CartCountChanged?.Invoke(this, EventArgs.Empty);
     }
-
+    // Get the object count from local storage
     public async Task<int> GetObjectCount()
     {
         // Fetch the data from local storage
